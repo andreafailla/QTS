6f96a168a96d4223b1fef1c9598de67641aae189
andreafailla <77196675+andreafailla@users.noreply.github.com>
2021-07-25 19:20:37 +0200

    Update QTS.py

--- QTS.py
+++ QTS.py
@@ -1,6 +1,6 @@
 """ @Author(s): Andrea Failla
-    @VersionID: 0.0
-    @LastUpdated: 25 lug 2021, 15:07:13
+    @VersionID: 0.2
+    @LastUpdated: 25 lug 2021, 19:20:06
 """
 import argparse, sys
 from collections import Counter
@@ -11,12 +11,14 @@ except ImportError:
     raise RuntimeError('This script assumes that nltk is installed on your system\
                         \nPlease install nltk, e.g. $pip install --user -U nltk')
 
+
 def lang_util(lang):
     if lang =='en':
         return 'english'
-    else:
+    elif lang == 'it':
         return 'italian'
 
+   
 def get_text(input):
     with open(input, encoding="utf-8") as f:
         text = f.read()
@@ -32,6 +34,12 @@ def tokenize_text(text, lang):
             tokensTOT.append(token)
     return sents, tokensTOT
 
+def n_chars(tokensTOT):
+    nchars = 0
+    for token in tokensTOT:
+        nchars += len(token)
+    return nchars
+        
 def content_words(tokensTOT, lang):
     sw = stopwords.words(lang)
     content = [word.lower() for word in tokensTOT if not (word.lower() in sw) and word.isalpha()]
@@ -39,14 +47,19 @@ def content_words(tokensTOT, lang):
     return content, funct 
 
 def PoS_dist(tokensTOT, lang):
+    if lang not in ['russian', 'english']:
+        dummy = {'x':0}
+        return dummy
     postags = nltk.pos_tag(tokensTOT, tagset='universal', lang=lang[:3])
     counts = Counter([j for i,j in postags])
     return dict(counts)
 
-def printall(lang, sents, tokensTOT, vocab, content, funct, PoScounts):
+def printall(lang, text, sents, tokensTOT, nchars, vocab, content, funct, PoScounts):
     if lang=='english':
         print('Sentences:', len(sents), '\nTokens:', len(tokensTOT))
         print('Token/Sent ratio:', round(len(tokensTOT)/float(len(sents)), 3))
+        print('Number of characters (whitespaces included):', len(text))
+        print('Number of characters (whitespaces excluded):', nchars)
         print()
         print('Type words:', len(vocab), '\nType/Token ratio:', round(len(vocab)/float(len(tokensTOT)), 3))
         print('Content Words:', len(content), '\nFunction Words:', len(funct))
@@ -59,14 +72,15 @@ def printall(lang, sents, tokensTOT, vocab, content, funct, PoScounts):
     elif lang=='italian':
         print('Frasi:', len(sents), '\nToken:', len(tokensTOT))
         print('Token per frase', round(len(tokensTOT)/float(len(sents)), 3))
+        print('Numero di caratteri (spazi esclusi):', nchars)
+        print('Numero di caratteri (spazi inclusi):', len(text))
         print()
         print('Vocabolario:', len(vocab), '\nType/Token ratio:', round(len(vocab)/float(len(tokensTOT)), 3))
         print('Parole contenuto:', len(content), '\nParole funzionali:', len(funct))
         print()
-        for tag, v in dict(PoScounts).items():
-            print("N.",tag + ":", v)
-            print('Media', tag, 'per frase:', round(v/float(len(sents)), 3))
+        print('Il PoS tagging in NLTK supporta solo inglese e russo :(')
         print()
+    
    
 
 def text_stats(input, lang):
@@ -74,16 +88,19 @@ def text_stats(input, lang):
     lang = lang_util(lang)
     # fetches text
     text = get_text(input) 
+
     # gets list of sentences and tokens
     sents, tokensTOT = tokenize_text(text, lang)
+    # gets number of characters (whitespaces excluded)
+    nchars = n_chars(tokensTOT)
     # gets list of type words
     vocab = list(set(tokensTOT))
     # gets list of content and function words
     content, funct = content_words(tokensTOT, lang)
     # gets dict -> {PoS:count}
     PoScounts = PoS_dist(tokensTOT, lang)
-    # print
-    printall(lang, sents, tokensTOT, vocab, content, funct, PoScounts)
+    # print to screen
+    printall(lang, text, sents, tokensTOT, nchars, vocab, content, funct, PoScounts)
 
 
 def main(argv):
